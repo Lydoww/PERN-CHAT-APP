b53d9f0046e711dad8e0203faaf902f32f7a9097
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-04-08 23:18:08 +1000

    AuthContext created and wrap around the App in main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import SignUp from "./pages/SignUp"
-import Login from "./pages/Login"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import { useAuthContex } from "./context/AuthContext";
 
 function App() {
-  
+  const { authUser, setAuthUser, isLoading } = useAuthContex;
+
+  if(isLoading) return null // optimize for a skeleton maybe
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/" element={<SignUp />}/>
-        <Route path="/" element={<Login />}/>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/"
+          element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/"
+          element={!authUser ? <Login /> : <Navigate to={"/"} />}
+        />
       </Routes>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
