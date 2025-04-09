04236684a423b60a07dfef9eb701d38f0a269777
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-04-10 08:50:44 +1000

    zustand installed + useConversation hook from it

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,14 +6,16 @@ import messagesRoutes from "./routes/message.route";
 import dotenv from "dotenv";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cookieParser());
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
